b0c3c192f96c75f24247fe5f4a3ca2a46c8684a5
Ryan Wiggins <ryanwiggins52@gmail.com>
2020-01-26 14:54:27 -0500

    added patch path

--- routes/posts.js
+++ routes/posts.js
@@ -46,4 +46,18 @@ router.delete('/:postId', async (req, res) => {
     res.json({ message: err })
   }
 })
+
+// update a post
+router.patch('/:postId', async (req, res) => {
+  try {
+    const updatedPost = await Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title } }
+    )
+    res.json(updatedPost)
+  } catch (err) {
+    res.json({ message: err })
+  }
+})
+
 module.exports = router
